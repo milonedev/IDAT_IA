65cbd5b214a8e596cb743643e90daef45a1f1ca8
Erick Milan <erickmilan1002@gmail.com>
2025-01-25 10:52:39 -0500

    add type of python

---
+++ hackaton01/estructuras_de_datos.py
@@ -0,0 +1,28 @@
+###### LISTAS ##########
+
+frutas = ["manzana", "platano", "cerezo"]
+
+frutas.append("naranja")
+frutas[2] = "cereza"
+
+# print(frutas)
+
+##### TUPLAS ###########
+
+# valores fijos
+dias_semana = ("lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo")
+
+#### DICCIONARIOS #####
+
+persona = {
+    "edad": 23,
+    "nombre": "Juan",
+    "fecha_de_nacimiento": "2002 - 06 - 09",
+    "ciudad": "Lima"
+}
+
+persona["edad"]=32
+persona["profesion"]="ingeniero"
+# print(persona.get("nombre"))
+# print(persona["profesion"])
+
---
+++ hackaton01/variables.py
@@ -0,0 +1,43 @@
+# diferentes tipo de variables para el lenguaje PYTHON
+edad = 23;
+# edad = "mi edad es: "
+# print(type(edad))
+
+# Tipo de asignaciones.
+
+## Asignacion simple
+edad = 23;
+
+## Asigancion multiple
+
+a, b, c = 1, 2, 3
+
+# print(a)
+# print(b)
+# print(c)
+
+## intercambio de valores
+
+a, b = b, a
+
+# print(a)
+# print(b)
+
+## Asignacion a multiples variables
+
+z = x = y = 4;
+
+# print(z)
+# print(x)
+# print(y)
+
+# Conversion de tipos(casteo o casting)
+
+numero_diez = 10
+
+string_numero_diez = str(numero_diez)
+
+# print(type(string_numero_diez))
+
+decimal_numero_diez = float(numero_diez)
+# print(decimal_numero_diez)
